72c1a2cc1747a39369bc562684390451bc8022ce
Dustin Dortch <me@dustindortch.com>
2024-11-07 14:35:25 -0500

    Fix location

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 resource "azurerm_service_plan" "sp" {
   name                = var.name
   location            = var.location
-  resource_group_name = var.location
+  resource_group_name = var.resource_group_name
   os_type             = "Linux"
   sku_name            = "Y1"
 }
